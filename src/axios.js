439207ce38d6acd5fe855c3205985d51c1e22826
Carlos Paredes Escobar <cparedes@inline.cl>
2026-05-16 21:09:11 -0400

    :bug: arreglada la dir de la

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://rd-pl-api-carlos-paredes.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
